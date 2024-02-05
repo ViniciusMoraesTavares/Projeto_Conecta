a7f17d15ea63db5ea1436ec49e513937354d8cd0
Vinicius Moraes Tavares <130687376+ViniciusMoraesTavares@users.noreply.github.com>
2024-02-05 11:55:53 -0300

    pagina login e cadastro

--- server/server.js
+++ server/server.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
 const app = express();
 
 app.use(express.json());
 
+// Middleware para adicionar Content Security Policy (CSP)
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      fontSrc: ["'self'", 'https://cdn.jsdelivr.net'],
+      scriptSrc: ["'self'", 'https://code.jquery.com', 'https://cdn.jsdelivr.net'],
+    },
+  })
+);
+
 // Servir arquivos estáticos da pasta 'public'
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 // Rota para a página home.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'Pages', 'home.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'Pages', 'home.html'));
+});
+
+// Rota para a página login.html
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'Pages', 'login.html'));
+});
+
+// Rota para a página cadastro.html
+app.get('/cadastro.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'Pages', 'cadastro.html'));
 });
 
 const PORT = process.env.PORT || 3000;
